b09bb8beab5a7242ca0de2e7a6b71eb2fca94996
Dilip <dilip_agheda@yahoo.com>
2019-04-25 09:26:37 +1000

    Header and Footer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
