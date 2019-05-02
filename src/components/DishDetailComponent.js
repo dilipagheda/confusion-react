b96d2aedcc73886aebeeea9d797bdf2d5da96e67
Dilip <dilip_agheda@yahoo.com>
2019-05-02 21:55:17 +1000

    DishDetail refactoring

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody,
     CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
-    
+import CommentForm from './CommentForm';
+
 function RenderDish(props) {
     if (props.dish != null)
     return(
@@ -33,12 +34,9 @@ function RenderComments({comments}) {
             );
         });
         return (
-            <div  className="col-12 col-md-5 m-1">
-                <h4>Comments</h4>
-                <ul class="list-unstyled">
-                    {commentsJSX}
-                </ul>
-            </div>
+            <ul class="list-unstyled">
+                {commentsJSX}
+            </ul>
         );
     }else{
         return (
@@ -65,9 +63,12 @@ const  DishDetail = (props) => {
             <div className="col-12 col-md-5 m-1">
                 <RenderDish dish={props.dish} />
             </div>
-            <div className="col-12 col-md-5 m-1">
+            <div  className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
                 <RenderComments comments={props.comments} />
+                <CommentForm />
             </div>
+            
         </div>
         </div>
     );
