0b2e8254998a18a3ec2d017ad08b77a6cf1493ab
Dilip <dilip_agheda@yahoo.com>
2019-09-22 19:26:10 +1000

    Fetch from Server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardText, CardBody,
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl.js';
 
 const minLength = (len) => (val) => val && (val.length >= len);
 const maxLength = (len) => (val) => val && (val.length < len);
@@ -92,7 +93,7 @@ function RenderDish(props) {
     if (props.dish != null)
     return(
         <Card>
-            <CardImg top src={props.dish.image} alt={props.dish.name} />
+            <CardImg top src={baseUrl +props.dish.image} alt={props.dish.name} />
             <CardBody>
               <CardTitle>{props.dish.name}</CardTitle>
               <CardText>{props.dish.description}</CardText>
